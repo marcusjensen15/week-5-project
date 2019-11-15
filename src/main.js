3b24b0d6532c977ca0c661d4e9f9de0e1113adc9
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-15 14:04:24 -0800

    Rough draft of DOM manipulation functions

--- src/main.js
+++ src/main.js
@@ -5,15 +5,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+  function lifeOutput(life){
+    if(life < 0){
+      return "Woah! you are beating the average life expectancy by: " + Math.abs(life);
+
+    }else {return "You have " + life + " years left to live.";}
+
+  }
+
+
     $("#ageInput").submit(function(event){
       event.preventDefault();
       let userInput = parseInt($("#userAgeInput").val());
       let planetAge = new PlanetAge(userInput);
-      console.log(planetAge);
+
+
       $("#output").text("Your age on Mercury is: " + planetAge.mercuryAge);
       $("#output2").text("Your age on Venus is: " + planetAge.venusAge);
       $("#output3").text("Your age on Mars is: " + planetAge.marsAge);
       $("#output4").text("Your age on Jupiter is: " + planetAge.jupiterAge);
+
+      $("#yearsToLive").text("Below is your life expectancy for each planet respectivly: ");
+
+      //below will print life expectancy for each planet
+      $("#output5").text("Mercury: " + lifeOutput(planetAge.mercuryYearsRemaining));
+      $("#output6").text("Venus: " + lifeOutput(planetAge.venusYearsRemaining));
+      $("#output7").text("Mars: " + lifeOutput(planetAge.marsYearsRemaining));
+      $("#output8").text("Jupiter: " +lifeOutput(planetAge.jupiterYearsRemaining));
 
 
 
