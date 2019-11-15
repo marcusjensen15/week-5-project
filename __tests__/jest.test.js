b43fc3cbe7cbf2c0763c4ad565ef9272f8e237fa
Marcus Jensen <marcusjensen1515@gmail.com>
2019-11-15 12:06:27 -0800

    Elimante unnecessary comments and clean up code. Need to create front end interface

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -1,40 +1,8 @@
 
 //Example testing for template//
 
-import { MyObject, MyObject2, PlanetAge } from './../src/dice-game.js'
+import { PlanetAge } from './../src/dice-game.js'
 
-// describe('MyObject', () => {
-//
-//     test('should record three numbers from the user', () => {
-//       var myObject= new MyObject(4,6,9);
-//       expect(myObject.number1).toEqual(4);
-//       expect(myObject.number2).toEqual(6);
-//       expect(myObject.number3).toEqual(9);
-//
-//
-//     });
-//
-// });
-//
-// describe('Numbers Multiply', () => {
-//
-//     test('should return the product of two numbers', () => {
-//       var myObject= new MyObject(4,6,9);
-//       expect(myObject.multiplyNumbers()).toEqual(24);
-//
-//     });
-//
-// });
-//
-// describe('Numbers Multiply', () => {
-//
-//     test('should return the product of two numbers', () => {
-//       var myObject= new MyObject2(4,6,9);
-//       expect(myObject.multiplyNumbers()).toEqual(10);
-//
-//     });
-//
-// });
 
 describe('Planet Age', () => {
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import { test } from  './dice-game.js';
+import { PlanetAge } from  './dice-game.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-function test2() {
-  console.log('this is the front end');
-} test2();
+// function test2() {
+//   console.log('this is the front end');
+// } test2();
 
 
 
 //Front-end logic
 
-// On the form submit, collect the user input in earth years. Create a new instance of that class based on that users input. 
+// On the form submit, collect the user input in earth years. Create a new instance of that class based on that users input.
